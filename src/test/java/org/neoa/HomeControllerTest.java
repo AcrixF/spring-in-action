1b1755a05c5a8f10a6246d7b8e40dc8d5eeab62b
Cristian Flores <acrixf@gmail.com>
2022-02-23 22:45:26 -0600

    sia-ch01 - fix test case

--- src/test/java/org/neoa/HomeControllerTest.java
+++ src/test/java/org/neoa/HomeControllerTest.java
@@ -23,7 +23,7 @@ public class HomeControllerTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
-                .andExpect(content().string(containsString("Welcome...")));
+                .andExpect(content().string(containsString("Welcome to...")));
     }
 
 }
